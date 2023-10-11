d2268d5ea633bc02921435d912ff0b72775fe3a6
Skanix <skanix.ytb@gmail.com>
2023-10-11 09:52:10 +0200

    Update

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ Site dynamique fait en Flask, SQL
 
 # Mettre en place le projet
 Installation 
+
+# Structure Base de données
+## Client:
+- id, name, lastname, email, password, size
+```SQL
+
+INSERT INTO `users` (`name`,`lastname`,`gender`,`admin`,`size`,`email`,`password`) VALUES ("Test", "account", "m",true,43, "test@test.test","testtest");
+```
--- views.py
+++ views.py
@@ -2,13 +2,24 @@ from flask import Flask, render_template, request
 import sqlite3 as sql
 app = Flask(__name__)
 
+# Variables
+#database = "database/nsi.db"
 
 @app.route('/')
 def index():
+
     return render_template('./views/index.html')
 
 @app.route('/fm')
 def fm():
+
+    con = sql.connect("database/nsi.db")
+    cursor = con.cursor()
+
+    requete = "SELECT * FROM `users`"
+    cursor.execute(requete)
+    con.commit()
+    print(cursor)
     return render_template('./views/fm.html')
 
 
